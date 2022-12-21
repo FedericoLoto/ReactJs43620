9a9e707bc836b620cce3e44271caaeff5b4ef1b8
FedericoLoto <110067581+FedericoLoto@users.noreply.github.com>
2022-12-21 11:47:35 -0300

    Delete Tienda Juego directory

--- Tienda Juego/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = ()=>{
-    return(
-        <>
-        <p className="piePagina">Tienda Juegos derechos reservados</p>
-        </>
-        )
-        
-};
-export default Footer;
--- Tienda Juego/src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import Logo from '../img/Logo.png'
-
-const LogoPag = ()=>{
-    return(
-        <img className='logo' src={Logo} alt="logo"/>
-    )
-};
-export default LogoPag;
